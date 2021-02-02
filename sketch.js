968bf9769538adf92aa73ce16cd4dff717029853
Vardaan-foot <76645082+Vardaan-foot@users.noreply.github.com>
2021-02-02 19:11:30 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var Cat,Cat_Image
+var Mouse,Mouse_image
+function preload() {
+Cat_Image=loadImage("cat1.png")
+Mouse_image=loadImage("mouse1.png")
+
+}
+
+function setup(){
+    createCanvas(1000,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
